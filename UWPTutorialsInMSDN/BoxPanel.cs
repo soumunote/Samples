a7f53d6034b9c2e970c376ff211a97490b53d52b
Note Soumu <soumunote@gmail.com>
2020-01-28 06:28:57 +0900

    Add UWPTutorialsInMSDN

---
+++ UWPTutorialsInMSDN/BoxPanel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPTutorialsInMSDN
+{
+    class BoxPanel : Panel
+    {
+        int maxrc, rowcount, colcount;
+        double cellwidht, cellheight, maxcellheight, aspectratio;
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            maxrc = (int)Math.Ceiling(Math.Sqrt(Children.Count));
+            aspectratio = availableSize.Width / availableSize.Height;
+
+            if (aspectratio < 1)
+            {
+                rowcount = maxrc;
+                colcount = (maxrc > 2 && Children.Count < maxrc * (maxrc - 1)) ? maxrc - 1 : maxrc;
+            }
+            else
+            {
+                rowcount = (maxrc > 2 && Children.Count < maxrc * (maxrc - 1)) ? maxrc - 1 : maxrc;
+                colcount = maxrc;
+            }
+
+            cellwidht = (int)Math.Floor(availableSize.Width / colcount);
+            cellheight = Double.IsInfinity(availableSize.Height) ? Double.PositiveInfinity : availableSize.Height / rowcount;
+
+            foreach (UIElement child in Children)
+            {
+                child.Measure(new Size(cellwidht, cellheight));
+                maxcellheight = (child.DesiredSize.Height > maxcellheight) ? child.DesiredSize.Height : maxcellheight;
+            }
+
+            return LimitUnboundedSize(availableSize);
+        }
+
+        Size LimitUnboundedSize(Size input)
+        {
+            if (Double.IsInfinity(input.Height))
+            {
+                input.Height = maxcellheight * colcount;
+                cellheight = maxcellheight;
+            }
+            return input;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            int count = 1;
+            double x, y;
+
+            foreach (UIElement child in Children)
+            {
+                x = (count - 1) % colcount * cellwidht;
+                y = ((int)(count - 1) / colcount) * cellheight;
+                Point anchorPoint = new Point(x, y);
+                child.Arrange(new Rect(anchorPoint, child.DesiredSize));
+                count++;
+            }
+
+            return finalSize;
+        }
+
+        public static readonly DependencyProperty UseOppositeRCRatioProperty =
+            DependencyProperty.Register("UseOppositeRCRatio", typeof(bool), typeof(BoxPanel), null);
+
+        public bool UseSquareCells
+        {
+            get => (bool)GetValue(UseOppositeRCRatioProperty);
+            set => SetValue(UseOppositeRCRatioProperty, value);
+        }
+    }
+}
---
+++ UWPTutorialsInMSDN/README.md
@@ -0,0 +1,10 @@
+﻿# UWPTutorialsInMSDN
+
+UWP学習のために、MSDNのドキュメントを入力している
+
+| Page | Description | URL |
+|------|-------------|-----|
+| MainPage | | [BoxPanel, an example custom panel](https://docs.microsoft.com/en-us/windows/uwp/design/layout/boxpanel-example-custom-panel) |
+| WeatherPage |  | [Tutorial: Use Grid and StackPanel to create a simple weather app](https://docs.microsoft.com/en-us/windows/uwp/design/layout/grid-tutorial) |
+| Form |  | [Form](https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/forms) |
+| NavigationViewPage |  | [NavigationView](https://docs.microsoft.com/en-us/windows/uwp/design/controls-and-patterns/navigationview) |
