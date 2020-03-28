08999e7ea036646562f0418415a5fbd8b0269c61
soumunote <soumunote@gmail.com>
2020-03-28 14:27:02 +0900

    Create backup-ESXi.cmd

---
+++ vmware/esxi6/backup-ESXi.cmd
@@ -0,0 +1,7 @@
+@rem ************************************************************
+@rem
+@rem  ESXi ホストのバックアップ
+@rem
+@rem ************************************************************
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\s-max\backup-ESXi.ps1
