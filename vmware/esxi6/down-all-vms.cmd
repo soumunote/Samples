5bf59ddece27a4485bcdd288da2cd357379a0612
soumunote <soumunote@gmail.com>
2020-03-28 14:35:35 +0900

    Update down-all-vms.cmd

--- vmware/esxi6/down-all-vms.cmd
+++ vmware/esxi6/down-all-vms.cmd
@@ -10,7 +10,8 @@
 @rem ************************************************************
 
 set PASSWORD=P@ssw0rd
+set TOOLDIR=c:\tooldir
 
-date /t >> c:\s-max\down-all-vms.start.txt
-time /t >> c:\s-max\down-all-vms.start.txt
-c:\s-max\tools\psexec.exe \\localhost -h -u Administrator -p %PASSWORD% -accepteula C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\s-max\down-all-vms.ps1 >> c:\s-max\down-all-vms.log.txt
+date /t >> %TOOLDIR%\down-all-vms.start.txt
+time /t >> %TOOLDIR%\down-all-vms.start.txt
+%TOOLDIR%\psexec.exe \\localhost -h -u Administrator -p %PASSWORD% -accepteula C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\s-max\down-all-vms.ps1 >> c:\s-max\down-all-vms.log.txt
