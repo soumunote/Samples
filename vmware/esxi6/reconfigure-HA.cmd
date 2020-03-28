4d5b37da2a4654d787a73a6e096973b95c5c83c0
soumunote <soumunote@gmail.com>
2020-03-28 14:38:48 +0900

    Create reconfigure-HA.cmd

---
+++ vmware/esxi6/reconfigure-HA.cmd
@@ -0,0 +1,7 @@
+@rem ************************************************************
+@rem
+@rem  HAを再構成する
+@rem
+@rem ************************************************************
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe C:\s-max\reconfigure-HA.ps1
