9268ee566e66180dffb4d013195e59a87a735d3a
Note Soumu <soumunote@gmail.com>
2021-04-26 19:57:00 +0900

    やっと webpack-dev-server が動いた

--- React/create-environment-step-by-step/99-react-bootstrap/webpack.config.js
+++ React/create-environment-step-by-step/99-react-bootstrap/webpack.config.js
@@ -8,6 +8,29 @@ module.exports = {
     filename: 'bundle.js',
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      template: `${path.resolve(__dirname, './public')}/index.html`,
+    })
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react'
+            ]
+          }
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist/'),
+    port: 9000,
+  },
 }
