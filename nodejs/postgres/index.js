c19e36038d6301de79f2784b8e8657774f663afc
Note Soumu <soumunote@gmail.com>
2021-05-29 09:56:57 +0900

    Bootstrap の入門を追加

--- nodejs/postgres/index.js
+++ nodejs/postgres/index.js
@@ -1,14 +1,17 @@
-import pg from 'pg';
-import path from 'path';
-import { readFile } from  'fs/promises';
+const { Pool, Client } = require('pg');
+const path = require('path');
+const { readFile } = require('fs/promises');
 
-const settings = path.join(__dirname, 'settings.json');
+(async () => {
+  
+  const settings = JSON.parse(await readFile(path.join(__dirname, 'settings.json'), 'utf8'));
 
-const client = new pg.Client(await readFile(settings));
-await client.connect();
-const result = await client.query('select * from tm_users');
-for (const row of result.rows) {
-  console.log(row['name']);
-}
+  const client = new Client(settings);
+  await client.connect();
 
+  const result = await client.query('select * from tm_user');
+  for (const row of result.rows) {
+    console.log(row['name']);
+  }
 
+})();
