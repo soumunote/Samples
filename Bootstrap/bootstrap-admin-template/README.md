9a573f2949d2ce8835d1c5b31d612c6cdb45bc8e
Note Soumu <soumunote@gmail.com>
2021-06-07 01:13:31 +0900

    Bootstrap サンプル完了

--- Bootstrap/bootstrap-admin-template/README.md
+++ Bootstrap/bootstrap-admin-template/README.md
@@ -1,2 +1,15 @@
 # Bootstrap 5 admin template
 [Bootstrap 5 admin template](https://mdbootstrap.com/snippets/standard/mdbootstrap/2897359)の写経
+
+# 学習事項
+## `margin`,`padding` に関するスタイル
+`me-1`, `ms-2` 等の意味不明スタイルに関する調査  
+- Bootstrap ドキュメントに答えがあった  
+  [Docs](https://getbootstrap.com/docs/5.0/getting-started/introduction/) / [Utilities](https://getbootstrap.com/docs/5.0/utilities/api/) / [Spacing](https://getbootstrap.com/docs/5.0/utilities/spacing/)  
+- 若干古めではあるが体系的な記載がある  
+  [CSS](https://getbootstrap.com/docs/3.4/css/#grid-example-basic)
+
+## Font Awesome  
+良い感じのWEBフォント...と関係無いがフォントつながりで。  
+- [Font Awsome](https://fontawesome.com)
+- [ふぉんときゅーとがーる](http://font.cutegirl.jp)
--- Bootstrap/bootstrap-admin-template/chart.js
+++ Bootstrap/bootstrap-admin-template/chart.js
@@ -1,40 +1,45 @@
-const ctx = document.getElementById("myChart");
+document.addEventListener('DOMContentLoaded', () => {
 
-const myChart = new CharacterData(ctx, {
-  type: "line",
-  data: {
-    labels: [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ],
-    datasets: [
-      {
-        data: [15339, 21345, 10403, 24003, 23489, 24092, 12034],
-        lineTension: 0,
-        backgroundColor: "transparent",
-        borderColor: "#007bff",
-        borderWidth: 4,
-        pointBackgroundColor: "#007bff"
-      },
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [
+  const ctx = document.getElementById("myChart");
+
+  const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ],
+      datasets: [
         {
-          ticks: {
-            beginAtZero: false,
-          }
-        }
+          data: [15339, 21345, 18403, 24003, 23489, 24092, 12034],
+          lineTension: 0,
+          backgroundColor: "transparent",
+          borderColor: "#007bff",
+          borderWidth: 4,
+          pointBackgroundColor: "#007bff"
+        },
       ]
     },
-    legend: {
-      display: false,
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: false,
+            }
+          }
+        ]
+      },
+      legend: {
+        display: false,
+      }
     }
-  }
+  });
+
 });
+
