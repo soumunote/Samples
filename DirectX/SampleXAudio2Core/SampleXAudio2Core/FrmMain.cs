f90dc2888e7a042fa92685ac0c3124e3a690f8ba
Note Soumu <soumunote@gmail.com>
2021-10-21 19:00:53 +0900

    Add SampleXAudio2Core

---
+++ DirectX/SampleXAudio2Core/SampleXAudio2Core/FrmMain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+public partial class FrmMain : Form
+{
+    public FrmMain()
+    {
+        InitializeComponent();
+    }
+
+    private void cmdPlay_Click(object sender, EventArgs e)
+    {
+        (new SoundPlayer()).Play(txtFileName.Text);
+    }
+}
---
+++ DirectX/SampleXAudio2Core/SampleXAudio2Core/SoundPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+using SharpDX.XAudio2;
+
+class SoundPlayer
+{
+    private XAudio2 xaDevice;
+    private MasteringVoice xaMaster;
+
+    public SoundPlayer()
+    {
+        xaDevice = new XAudio2();
+        xaMaster = new MasteringVoice(xaDevice);
+    }
+
+    public void Play(string soundFileName)
+    {
+        using (var xaStream = new SharpDX.Multimedia.SoundStream(File.OpenRead(soundFileName)))
+        {
+            var xaSource = new SourceVoice(xaDevice, xaStream.Format);
+            var xaBuffer = new AudioBuffer()
+            {
+                AudioBytes = (int)xaStream.Length,
+                Stream = xaStream,
+                LoopCount = XAudio2.NoLoopRegion,
+                LoopBegin = 0,
+                LoopLength = 0,
+                PlayBegin = 0,
+                PlayLength = 0,
+                Flags = BufferFlags.EndOfStream
+            };
+            xaStream.Close();
+
+            if (xaSource != null)
+            {
+                xaSource.SubmitSourceBuffer(xaBuffer, xaStream.DecodedPacketsInfo);
+                xaSource.SetVolume(1.0f);
+                xaSource.Start();
+            }
+        }
+    }
+}
